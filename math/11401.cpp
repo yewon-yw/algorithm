f2d221f49964a5f4ea6860339adaea77b93585d9
yewon Kim <101886039+yewon-yw@users.noreply.github.com>
2022-04-07 22:50:16 +0900

    이항 계수 3

---
+++ math/11401.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+long long N, K, A = 1, B = 1, half;
+long long mod = 1000000007;
+
+long long solve(int x)
+{
+	if (x == 0) return 1;
+
+	if (x % 2 == 1) return B * solve(x - 1) % mod;
+	else
+	{
+		half = solve(x / 2);
+		return half * half % mod;
+	}
+}
+
+int main()
+{
+	cin >> N >> K;
+	for (int i = N; i >= N - K + 1; i--) A = (A * i) % mod;
+	for (int i = 1; i <= K; i++) B = (B * i) % mod;
+	B = solve(mod - 2);
+	cout << A * B % mod;
+}
