3789c42999613dfa92b5c97101d285ce4c6782b5
yewon Kim <101886039+yewon-yw@users.noreply.github.com>
2022-05-21 17:22:47 +0900

    하노이 탑 이동 순서

---
+++ recursion/11729.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void hanoi(int n, int from, int by, int to)
+{
+	if (n == 1) cout << from << " " << to << "\n";
+	else {
+		hanoi(n - 1, from, to, by);
+		cout << from << " " << to << "\n";
+		hanoi(n - 1, by, from, to);
+	}
+}
+
+int main() {
+	int n;
+	cin >> n;
+	cout << (1 << n) - 1 << "\n";
+	hanoi(n, 1, 2, 3);
+}
