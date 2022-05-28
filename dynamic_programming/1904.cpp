4405d80ef560dcfa3dc49deec2b123f2e68ddf9c
yewon Kim <101886039+yewon-yw@users.noreply.github.com>
2022-05-28 15:39:26 +0900

    01타일

---
+++ dynamic_programming/1904.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+long long dp[1000001];
+int main() {
+	int n;
+	cin >> n;
+	dp[1] = 1; dp[2] = 2;
+	for (int i = 3; i <= n; i++) {
+		dp[i] = (dp[i - 1] + dp[i - 2]) % 15746;
+	}
+	cout << dp[n];
+}
